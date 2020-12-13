565781578e5f5b329be82efd5666d2fb7f9ddfea
Okhtay Sattari <33090650+okhsat@users.noreply.github.com>
2020-12-14 00:59:57 +0300

    Add files via upload

--- client/IndexController.php
+++ client/IndexController.php
@@ -14,14 +14,16 @@ class IndexController extends Controller
     {
         try {
             $this->view->pick('index');
-            $this->view->setVar('coupon_url',    isset($this->config->api->coupon_url)    ? $this->config->api->coupon_url    : '');
-            $this->view->setVar('cities_url',    isset($this->config->api->cities_url)    ? $this->config->api->cities_url    : '');
-            $this->view->setVar('user_url',      isset($this->config->api->user_url)      ? $this->config->api->user_url      : '');
-            $this->view->setVar('token_url',     isset($this->config->api->token_url)     ? $this->config->api->token_url     : '');
-            $this->view->setVar('auth_url',      isset($this->config->api->auth_url)      ? $this->config->api->auth_url      : '');
-            $this->view->setVar('base_url',      isset($this->config->api->base_url)      ? $this->config->api->base_url      : '');
-            $this->view->setVar('client_id',     isset($this->config->api->client_id)     ? $this->config->api->client_id     : '');
-            $this->view->setVar('client_secret', isset($this->config->api->client_secret) ? $this->config->api->client_secret : '');
+            
+            $this->view->setVar('prf_upload_url', isset($this->config->api->prf_upload_url) ? $this->config->api->prf_upload_url : '');
+            $this->view->setVar('coupon_url',     isset($this->config->api->coupon_url)     ? $this->config->api->coupon_url     : '');
+            $this->view->setVar('cities_url',     isset($this->config->api->cities_url)     ? $this->config->api->cities_url     : '');
+            $this->view->setVar('user_url',       isset($this->config->api->user_url)       ? $this->config->api->user_url       : '');
+            $this->view->setVar('token_url',      isset($this->config->api->token_url)      ? $this->config->api->token_url      : '');
+            $this->view->setVar('auth_url',       isset($this->config->api->auth_url)       ? $this->config->api->auth_url       : '');
+            $this->view->setVar('base_url',       isset($this->config->api->base_url)       ? $this->config->api->base_url       : '');
+            $this->view->setVar('client_id',      isset($this->config->api->client_id)      ? $this->config->api->client_id      : '');
+            $this->view->setVar('client_secret',  isset($this->config->api->client_secret)  ? $this->config->api->client_secret  : '');
 
         } catch (Exception $e) {
             $this->flash->error( $e->getMessage() );
--- client/config.php
+++ client/config.php
@@ -9,14 +9,15 @@ return new Config(
         ],
         
         'api' => [
-            'coupon_url'    => BASE_URL.'/api/coupon',
-            'cities_url'    => BASE_URL.'/api/cities',
-            'user_url'      => BASE_URL.'/api/user',
-            'token_url'     => BASE_URL.'/api/auth/v1/oauth/token/validate',
-            'auth_url'      => BASE_URL.'/api/auth/v1/oauth/token',
-            'base_url'      => BASE_URL,
-            'client_id'     => 'general-3426',
-            'client_secret' => 'iytds43wert67y8urrdfg6766dfg676drf7'
+            'prf_upload_url' => BASE_URL.'/api/prf-upload',
+            'coupon_url'     => BASE_URL.'/api/coupon',
+            'cities_url'     => BASE_URL.'/api/cities',
+            'user_url'       => BASE_URL.'/api/user',
+            'token_url'      => BASE_URL.'/api/auth/v1/oauth/token/validate',
+            'auth_url'       => BASE_URL.'/api/auth/v1/oauth/token',
+            'base_url'       => BASE_URL,
+            'client_id'      => 'general-3426',
+            'client_secret'  => 'iytds43wert67y8urrdfg6766dfg676drf7'
         ]
     ]    
 );
